9853a6c124a5c32cdf320d9afac4bd181d330d20
Avula ragavendra <147192093+Raghava1329@users.noreply.github.com>
2025-12-28 12:42:07 +0530

    modified the server.js after irfan conflict

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,3 @@
-import dotenv from 'dotenv';
-dotenv.config();
 import express from 'express';
 import serverless from 'serverless-http';
 import cors from 'cors';
@@ -37,6 +35,7 @@ import astrologerRoutes from './routes/astrologerRoutes.js';
 import priestAssignmentRoutes from './routes/priestAssignmentRoutes.js';
 import priestAvailabilityRoutes from './routes/priestAvailabilityRoutes.js';
 
+dotenv.config();
 
 const app = express();
 
